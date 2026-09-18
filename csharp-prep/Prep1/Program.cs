2ff78c112aadd5b5495b782e7326423cbad71dd0
griffithscal6-prog <griffithscal6@gmail.com>
2026-09-18 14:30:01 -0600

    Update Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Prompt the user for First Name
+        Console.WriteLine("What is your First Name? ");
+
+        //Read the input and store it as a variable
+        string firstName = Console.ReadLine();
+
+        //Prompt the user for Last Name
+        Console.WriteLine("What is your Last Name? ");
+
+        //Read the input and store it as a variable
+        string lastName = Console.ReadLine();
+
+        //Use the input as instructed
+        Console.WriteLine($"Your Name is {lastName}, {firstName} {lastName}.");
+
     }
 }
