4c1617f6ab0cbd65f4d63e204e580674de6479bd
griffithscal6-prog <griffithscal6@gmail.com>
2026-09-18 15:18:20 -0600

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        //Prompt the user for their grade
+        Console.WriteLine("What is your Grade? ");
+        string grade = Console.ReadLine();
+        int x = int.Parse(grade);
+        if (x >= 90)
+        {
+            letter = "A";
+        }
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+        else if (x >= 70)
+        {
+            letter = "C";
+        }
+        else if (x >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (x >= 70)
+        {
+            Console.WriteLine($"Congratulations! You passed the course with a {letter}!");
+        }
+        else
+        {
+            Console.WriteLine($"Unfortunately, You failed the course with a {letter}. :( Better luck Next time!");
+        }
     }
 }
